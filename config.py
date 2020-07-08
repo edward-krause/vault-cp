a4a054a9d681f3a1753fb6a502e1f36fe2afd3f3
krausee <krausee@email.chop.edu>
2020-07-08 11:02:37 -0400

    allow different secret paths from one vault instance to another

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
+
 old_url = 'https://vault-old.example.com'
 old_token = 'old-token'
 new_url = 'https://vault-new.example.com'
 new_token = 'new-token'
 
-secrets = [
-    'secret/path/example'
-]
+secrets = {
+    'secret1/path1/example1':'secret2/path2/example2',
+    'secret3/path3/example3':'secret4/path4/exampel4'
+}
--- vault-cp.py
+++ vault-cp.py
@@ -7,6 +7,8 @@ client_old = hvac.Client(url=cfg.old_url, token=cfg.old_token)
 client_new = hvac.Client(url=cfg.new_url, token=cfg.new_token)
 
 for secret in cfg.secrets:
+    old_path = secret
+    new_path = cfg.secrets[secret]
     data = client_old.read(secret).get('data')
-    client_new.write(secret, **data)
-    print("Copied " + secret)
+    client_new.write(new_path, **data)
+    print("Copied " + old_path + ' to ' + new_path)
